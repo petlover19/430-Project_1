6d557ff9bffef75b908f7ab793dbcfa818f7051a
petlover19 <varpu19@yahoo.co.jp>
2020-10-03 13:46:47 -0400

    first commit

---
+++ src/jsonResponses.js
@@ -0,0 +1,60 @@
+// Note this object is purely in memory
+// When node shuts down this will be cleared.
+// Same when your heroku app shuts down from inactivity
+// We will be working with databases in the next few weeks.
+const recipes = {};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const getRecipes = (request, response) => {
+  const responseJSON = {
+    recipes,
+  };
+
+  respondJSON(request, response, 200, responseJSON);
+};
+
+const addRecipe = (request, response, body) => {
+  const responseJSON = {
+    message: 'all fields are required required',
+  };
+
+  if (!body.name || !body.ingredients || !body.directions) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON); // 400=bad request
+  }
+
+  // got all params entered
+  let responseCode = 201; // "created"
+  if (recipes[body.name]) { // recipe exists
+    responseCode = 204; // updating, so "no content"
+  } else {
+    recipes[body.name] = {}; // make a new recipe
+  }
+
+  // update or initialize values, as the case may be
+  recipes[body.name].name = body.name;
+  recipes[body.name].ingredients = body.ingredients;
+  recipes[body.name].directions = body.directions;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode); // this is for 204, a "no content" header
+};
+
+module.exports = {
+  getRecipes,
+  addRecipe,
+};
---
+++ src/server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addRecipe') {
+    const body = [];
+
+    // https://nodejs.org/api/http.html
+    request.on('error', (err) => {
+      console.dir(error);
+      response.statusCode = 400;
+      response.end();
+    });
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      jsonHandler.addRecipe(request, response, bodyParams);
+    });
+  }
+};
+
+const handleGet = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getRecipes') {
+    jsonHandler.getRecipes(request, response);
+  } else {
+    htmlHandler.getIndex(request, response);
+  }
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
