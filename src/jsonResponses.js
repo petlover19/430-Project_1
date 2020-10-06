c9cab83c02e3fd9602a02802a3fed13fb9b4b8f5
petlover19 <varpu19@yahoo.co.jp>
2020-10-06 19:15:26 -0400

    submit not working

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,58 +1,58 @@
 const recipes = {};
 
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' });
-    response.write(JSON.stringify(object));
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-    response.writeHead(status, { 'Content-Type': 'application/json' });
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
 };
 
 const getRecipes = (request, response) => {
-    console.log("getRecipes");
-    const responseJSON = {
-        recipes,
-    };
-    console.log("recipes", recipes);
-    respondJSON(request, response, 200, responseJSON);
+  console.log('getRecipes');
+  const responseJSON = {
+    recipes,
+  };
+  console.log('recipes', recipes);
+  respondJSON(request, response, 200, responseJSON);
 };
 
 const addRecipe = (request, response, body) => {
-    console.log('recipe has been added');
-    const responseJSON = {
-        message: 'all fields are required required',
-    };
+  console.log('recipe has been added');
+  const responseJSON = {
+    message: 'all fields are required required',
+  };
 
-    if (!body.name || !body.ingredients || !body.directions) {
-        responseJSON.id = 'missingParams';
-        return respondJSON(request, response, 400, responseJSON); // 400=bad request
-    }
+  if (!body.name || !body.ingredients || !body.directions) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON); // 400=bad request
+  }
 
-    // got all params entered
-    let responseCode = 201; // "created"
-    if (recipes[body.name]) { // recipe exists
-        responseCode = 204; // updating, so "no content"
-    } else {
-        recipes[body.name] = {}; // make a new recipe
-    }
+  // got all params entered
+  let responseCode = 201; // "created"
+  if (recipes[body.name]) { // recipe exists
+    responseCode = 204; // updating, so "no content"
+  } else {
+    recipes[body.name] = {}; // make a new recipe
+  }
 
-    // update or initialize values, as the case may be
-    recipes[body.name].name = body.name;
-    recipes[body.name].ingredients = body.ingredients;
-    recipes[body.name].directions = body.directions;
+  // update or initialize values, as the case may be
+  recipes[body.name].name = body.name;
+  recipes[body.name].ingredients = body.ingredients;
+  recipes[body.name].directions = body.directions;
 
-    if (responseCode === 201) {
-        responseJSON.message = 'Created Successfully';
-        return respondJSON(request, response, responseCode, responseJSON);
-    }
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
 
-    return respondJSONMeta(request, response, responseCode); // this is for 204, a "no content" header
+  return respondJSONMeta(request, response, responseCode); // this is for 204, a "no content" header
 };
 
 module.exports = {
-    getRecipes,
-    addRecipe,
+  getRecipes,
+  addRecipe,
 };
